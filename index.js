6c6b34ae0c2205797ad8e83bf2930ae50dc8ab18
Jed Watson <jed.watson@me.com>
2015-08-15 01:45:13 +1000

    Return an array when it's passed

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function truthy(val) { return val; }
 function trim(str) { return str.trim(); }
 
 function listToArray (str, delimiter) {
+	if (Array.isArray(str)) {
+		return str;
+	}
 	if (!str || typeof str !== 'string') {
 		return [];
 	}
